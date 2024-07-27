d269507fa9642fd95ec4a5495048206fd004a244
Odiovock <64711277+Odiovock@users.noreply.github.com>
2024-07-27 11:34:35 -0400

    Tile list .map rendering

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Tile } from "../tile/Tile";
 
-export const TileList = () => {
+export const TileList = ({contactList}) => {
   return (
     <div>
-      
+      <ul>
+        {contactList.map((contact, index) => {
+          const {name , ...rest} = contact;
+          <Tile key={index} name={name} description={rest}/>
+        })}
+      </ul>
     </div>
   );
 };
